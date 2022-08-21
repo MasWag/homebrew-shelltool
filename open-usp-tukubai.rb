d9dd87e471dc5431787fbc429d2a9e808908b069
papiron <Katsuya.TASHIRO@gmail.com>
2022-08-21 18:08:39 +0900

    インストール先ディレクトリを指定する環境変数を、LOCALBASEからPREFIXに変更。

--- open-usp-tukubai.rb
+++ open-usp-tukubai.rb
@@ -2,11 +2,11 @@ class OpenUspTukubai < Formula
   desc "Tukubai command set for open source version"
   homepage "https://uec.usp-lab.com/TUKUBAI/CGI/TUKUBAI.CGI?POMPA=DOWNLOAD"
   url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", revision: "be1fdd44f8a9a7d27e92eb8d852f1af067becca1"
-  version "20220819"
-  head "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", branch: "master"
+  version "20220821"
+  # head "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", branch: "master"
 
   def install
-    ENV.append "LOCALBASE", prefix
+    ENV.append "PREFIX", prefix
     system "make", "install"
   end
 
