90395255451fdd2d46b4f1531ce7d6130bb83ad6
papiron <Katsuya.TASHIRO@gmail.com>
2022-09-26 21:33:07 +0900

    Update to 20220926

--- open-usp-tukubai.rb
+++ open-usp-tukubai.rb
@@ -1,8 +1,8 @@
 class OpenUspTukubai < Formula
   desc "Tukubai command set for open source version"
   homepage "https://uec.usp-lab.com/TUKUBAI/CGI/TUKUBAI.CGI?POMPA=DOWNLOAD"
-  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", revision: "bf506bf4ca075c4f8a044f28c8b89348a42decd1"
-  version "20220906"
+  url "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", revision: "4bc409c11f0180a40728a55b336b93cef7220ce5"
+  version "20220926"
   # head "https://github.com/usp-engineers-community/Open-usp-Tukubai.git", branch: "master"
 
   def install
